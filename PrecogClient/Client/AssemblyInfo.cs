f8e5c4abf87add944d9f4a87934a808e90170942
Derek Chen-Becker <github@chen-becker.org>
2013-04-18 18:25:53 -0600

    Added missing AssemblyInfo.cs to add version info to DLL

---
+++ PrecogClient/Client/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("Precog Client for .Net")]
+[assembly: AssemblyDescription("The .Net client for accessing the Precog analytics platform")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ReportGrid, Inc.")]
+[assembly: AssemblyProduct("Precog Client for .Net")]
+[assembly: AssemblyCopyright("Copyright 2013, ReportGrid, Inc.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("1.0.*")]
+
+// The following attributes are used to specify the signing key for the assembly, 
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
+
